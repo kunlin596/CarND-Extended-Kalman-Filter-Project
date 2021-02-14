74f813fecc6d63413ad59a5f432a64041fc969c3
Kun Lin <kun.lin.596@gmail.com>
2021-02-14 20:11:49 +0900

    (feat) Complete EKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,41 +1,63 @@
 #include "kalman_filter.h"
+#include "tools.h"
+#include "logger.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/* 
- * Please note that the Eigen library does not initialize 
+/*
+ * Please note that the Eigen library does not initialize
  *   VectorXd or MatrixXd objects with zeros upon creation.
  */
 
-KalmanFilter::KalmanFilter() {}
+void KalmanFilter::Predict(double dt) {
 
-KalmanFilter::~KalmanFilter() {}
+    // Update F
+    F_(0, 2) = F_(1, 3) = dt;
+    // Using linear motion model for all measurements
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-    x_ = x_in;
-    P_ = P_in;
-    F_ = F_in;
-    H_ = H_in;
-    R_ = R_in;
-    Q_ = Q_in;
+    // Update Q
+    static constexpr double noise_ax = 9.0,
+                            noise_ay = 9.0;
+
+    const double dt4 = std::pow(dt, 4) / 4.0,
+                 dt3 = std::pow(dt, 3) / 2.0,
+                 dt2 = std::pow(dt, 2);
+
+    Q_ <<
+        dt4 * noise_ax , 0.0            , dt3 * noise_ax , 0.0             ,
+        0.0            , dt4 * noise_ay , 0.0            , dt3 * noise_ay  ,
+        dt3 * noise_ax , 0.0            , dt2 * noise_ax , 0.0             ,
+        0.0            , dt3 * noise_ay , 0.0            , dt2 * noise_ay  ;
+
+    // Predict next state and covariance
+    x_ = F_ * x_;
+    P_ = F_ * P_ * F_.transpose() + Q_;
+    BOOST_LOG_TRIVIAL(debug) << "Predict: x=" << x_.transpose().format(HeavyFmt);
+//    BOOST_LOG_TRIVIAL(debug) << "   P=\n" << P_.format(HeavyFmt);
 }
 
-void KalmanFilter::Predict() {
-    /**
-     * TODO: predict the state
-     */
+void KalmanFilter::Update(const Eigen::Vector2d &z) {
+    const Eigen::Vector2d y = z - H_ * x_;
+    const Eigen::Matrix2d S = H_ * P_ * H_.transpose() + R_;
+    const Eigen::Matrix<double, 4, 2> K = P_ * H_.transpose() * S.inverse();
+    x_ = x_ + K * y;
+    P_ = P_ - K * H_ * P_;
 }
 
-void KalmanFilter::Update(const VectorXd &z) {
-    /**
-     * TODO: update the state by using Kalman Filter equations
-     */
-}
+void KalmanFilter::UpdateEKF(const Eigen::Vector3d &z) {
+    Eigen::Matrix<double, 3, 4> Hj = Tools::CalculateJacobian(x_);
+    Eigen::Vector3d z_pred = Tools::ConvertCartesianToPolar(x_);
 
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
-    /**
-     * TODO: update the state by using Extended Kalman Filter equations
-     */
+    // NOTE: Handle a case where the measured angle changes from M_PI to -M_PI.
+    // FIXME: Is there better way to flip the angle?
+    if (z_pred[1] < -M_PI * 0.9 and z[1] > M_PI * 0.9) {
+        z_pred[1] = 2 * M_PI - std::abs(z_pred[1]);
+    }
+
+    Eigen::Vector3d y = z - z_pred;
+    Eigen::Matrix3d S = Hj * P_ * Hj.transpose() + R_;
+    Eigen::Matrix<double, 4, 3> K = P_ * Hj.transpose() * S.inverse();
+    x_ = x_ + K * y;
+    P_ = P_ - K * Hj * P_;
 }
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -8,46 +8,34 @@ public:
     /**
      * Constructor
      */
-    KalmanFilter();
+    KalmanFilter() {};
 
     /**
      * Destructor
      */
-    virtual ~KalmanFilter();
-
-    /**
-     * Init Initializes Kalman filter
-     * @param x_in Initial state
-     * @param P_in Initial state covariance
-     * @param F_in Transition matrix
-     * @param H_in Measurement matrix
-     * @param R_in Measurement covariance matrix
-     * @param Q_in Process covariance matrix
-     */
-    void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-              Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
+    virtual ~KalmanFilter() {};
 
     /**
      * Prediction Predicts the state and the state covariance
      * using the process model
-     * @param delta_T Time between k and k+1 in s
+     * @param dt Time between k and k+1 in s
      */
-    void Predict();
+    void Predict(double dt);
 
     /**
      * Updates the state by using standard Kalman Filter equations
      * @param z The measurement at k+1
      */
-    void Update(const Eigen::VectorXd &z);
+    void Update(const Eigen::Vector2d &z);
 
     /**
      * Updates the state by using Extended Kalman Filter equations
      * @param z The measurement at k+1
      */
-    void UpdateEKF(const Eigen::VectorXd &z);
+    void UpdateEKF(const Eigen::Vector3d &z);
 
     // state vector
-    Eigen::VectorXd x_;
+    Eigen::Vector4d x_;
 
     // state covariance matrix
     Eigen::MatrixXd P_;
